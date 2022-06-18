184a9cf09e1b95bdde318aaa3790f9d82646a166
JP6780 <kpalinski99@gmail.com>
2022-06-18 17:02:54 +0200

    User Prompt + API token

--- property_notifier.py
+++ property_notifier.py
@@ -1,27 +1,60 @@
+import requests
+
 class User:
     def __init__(self):
         self.user_type=None
-        self.suburb=[]
-        self.property_type=[]
+        self.suburbs=[]
+        self.property_types=[]
         self.min_price=None
         self.max_price=None
+        self.bedrooms=None
+        self.bathrooms=None
         self.area=None
-        self.date_posted=None #Earliest Date Posting when searching for property
+        self.date_posted=None #earliest date posting when searching for property
         self.car_spaces=None
-        self.desired_demographic=None
         self.target_appreciation=None
         self.target_rentalyield=None
     
     def prompt(self):
-        pass
+        
+        self.user_type=input('Are you a investor, home buyer or a renter? Enter One ').lower().title()
+
+        num_suburbs=int(input('How many suburbs are you interested in? '))
+        self.suburbs.extend(input(f'Please enter the {num_suburbs} you are interested in: ').lower().title().replace('And',',').split(','))
+        
+        self.property_types=input('Which of the following are you interested in: Houses, Apartments, Townhouses? ').lower().title().replace('And',',').replace(' ','').split(',')
+        self.min_price=int(input('Enter minimum price you are willing to pay for property ($AUD): ').strip(','))
+        self.max_price=int(input('Enter maximum price you are willing to pay for property ($AUD): ').strip(','))
+        self.bedrooms=int(input('Enter number of bedrooms '))
+        self.bathrooms=int(input('Enter number of bathrooms '))
+        
+        if 'Houses' in self.property_types:
+            self.area=int(input('Please enter minimum desired Land Area (sqm): ')) #on domain search only land area for houses is shown
+
+        self.date_posted=input('Latest posting date (yyyy-mm-dd)? ')
+        self.car_spaces=int(input('How many car spaces? Enter Number '))
+
+        if self.user_type in ['Home Buyer', 'Investor']:
+            self.target_appreciation=float(input('What is your desired appreciation on the property (%)? '))
+            self.target_rentalyield=float(input('What is your desired rental yield on the property (%)? '))
 
 class API:
     def __init__(self):
-        self.client_id=None
-        self.accesskey=None
+        self.url=' https://auth.domain.com.au/v1/connect/token'
+        self.client_id='client_4233d25eceb0e463018f9c809e1f43a8'
+        self.client_secret='secret_597027cbb4d6b1a839794fc6b71decb9'
+        self.scopes=['api_listings_read api_suburbperformance_read']
     
-    def authenticate(self):
-        pass
+    def generate_token(self):
+        auth_response=requests.post(self.url,{
+            'client_id':self.client_id,
+            'client_secret':self.client_secret,
+            'grant_type':'client_credentials',
+            'scope':self.scopes,
+            'Content-Type':'text/json'
+        })
+        json_response=auth_response.json()
+        self.access_token=json_response['access_token']
 
 class Property_Data:
     def __init__(self):
@@ -31,9 +64,6 @@ class Property_Data:
         pass
 
     def listings_info(self):
-        pass
-
-    def demographics(self):
         pass
 
     def statistics(self):
--- unit_test.py
+++ unit_test.py
@@ -4,8 +4,30 @@ from property_notifier import *
 
 class test_list(unittest.TestCase):
     
-    pass
+    def test_correct_attributes_home_buyer(self):
+        home_buyer=User()
+        home_buyer.prompt()
+        print(home_buyer.__dict__)
+        self.assertEqual(home_buyer.__dict__,{'user_type': 'Home Buyer', 'suburbs': ['Madeley','Darch', 'Two Rocks'], 
+        'property_types':['Houses', 'Apartments'],'min_price':400000, 'max_price':600000, 'bedrooms':3,
+        'bathrooms':2, 'area': 300,'date_posted': '2022-06-17', 'car_spaces':2, 
+        'desired_demographic': 'Under 20', 'target_appreciation': 5.1, 'target_rentalyield': 3.5})
+    
+    def test_correct_attributes_renter(self):
+        renter=User()
+        renter.prompt()
+        print(renter.__dict__)
+        self.assertEqual(renter.__dict__,{'user_type': 'Renter', 'suburbs': ['Madeley','Darch', 'Two Rocks'], 
+        'property_types':['Houses', 'Apartments'],'min_price':400000, 'max_price':600000, 'bedrooms':3,
+        'bathrooms':2, 'area': 300,'date_posted': '2022-06-17', 'car_spaces':2, 
+        'desired_demographic': 'Under 20', 'target_appreciation': None, 'target_rentalyield': None})
+    
+    def test_access_token(self):
+        domain_api=API()
+        domain_api.authenticate()
+        self.assertTrue(domain_api.access_token,domain_api.access_token!=None)
 
 
 if __name__=='__main__':
-    unittest.main()
+    test_access=test_list()
+    test_access.test_access_token()
